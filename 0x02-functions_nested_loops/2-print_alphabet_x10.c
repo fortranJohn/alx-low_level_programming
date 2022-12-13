99dee979aa87d3fecd52ebadd06eb70e623d96ff
Ogunbiyi Temitope <ogunbiyitemitopeayodele@gmail.com>
2022-12-13 14:01:40 -0800

    alphabet x10 commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+# include "main.h"
+
+/**
+ * 10 x alphabet - Entry point
+ *
+ * Return: Always (0)
+ */
+void print_alphabet_x10(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 97; b <= 122; b++)
+		{
+			_putchar(b);
+		}
+		_putchar('\n');
+	}
+}
