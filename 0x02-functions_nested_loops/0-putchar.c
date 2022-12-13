52a4a4b5537318906c0f44f4634cfaa80dd58cb7
Ogunbiyi Temitope <ogunbiyitemitopeayodele@gmail.com>
2022-12-13 13:15:28 -0800

    firsts

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-# include <main.h>
+# include "main.h"
 
 /**
  * main - Prints _putchar as a message.
@@ -7,15 +7,16 @@
  */
 int main(void)
 {
-	int str[] = {95, 112, 117, 116, 99, 72, 97, 114};
-	int count, sz;
-
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
-	{
-		_putchar(str[count]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
 
