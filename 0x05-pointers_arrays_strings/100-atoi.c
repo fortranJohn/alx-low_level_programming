637e296df25a1a5fd9353d5eef568807a3429b8f
Ogunbiyi Temitope <ogunbiyitemitopeayodele@gmail.com>
2022-12-19 15:41:11 -0800

    tasks int

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,24 +8,26 @@
  */
 int main(void)
 {
-	int n;
+	int sign;
+	unsigned int num;
+	char *temp;
 
-	n = _atoi("xd-xds5");
-	/*n = 1000000000;*/
-	printf("%d\n", n);
-	n = _atoi("-402");
-	printf("%d\n", n);
-	n = _atoi("          ------++++++-----+++++--98");
-	printf("%d\n", n);
-	n = _atoi("214748364");
-	printf("%d\n", n);
-	n = _atoi("0");
-	printf("%d\n", n);
-	n = _atoi("Suite 402");
-	printf("%d\n", n);
-	n = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
-	printf("%d\n", n);
-	n = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
-	printf("%d\n", n);
-	return (0);
+	temp = s;
+	num = 0;
+	sign = 1;
+	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
+	{
+		if (*temp == '-')
+		{
+			sign *= -1;
+		}
+		temp++;
+		if (*temp != '\0')
+		{
+			do {
+				num = num * 10 + (*temp - '0');
+				temp++} while (*temp >= '0' && *temp <= '9');
+		}
+		return (num * sign);
+	}
 }
